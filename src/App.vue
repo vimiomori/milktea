419318bedc9ff3946a484743b42bc7aef37ca208
vimiomori <vivian.muchen@gmail.com>
2020-03-25 16:04:04 +0900

    set up axios

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,14 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'Bujo',
+  data () {
+    return {
+      msg: 'Welcome to Bujo'
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
